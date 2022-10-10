d90c3be4c4f4bb70f535c514b87bfc12d607e98a
gfishbone <gfishbone@gmail.com>
2022-10-10 10:15:02 -0400

    Update index.js

--- src/pages/Home/Third/index.js
+++ src/pages/Home/Third/index.js
@@ -9,56 +9,41 @@ import Accordion from "../../../components/Accordion";
 import Roadmap from "../../../components/Roadmap";
 
 const roadmapQ1 = {
-    title: 'Q1, 2022',
+    title: 'Q4, 2022',
     img: 'calendar.png',
     contents: [
         {
             img: 'map_lunched.png',
-            text: 'Launch NFTBook Minter on Polygon (Accessible to PageDAO Members ONLY)'
+            text: 'Launch Ye Olde Minting Press on Polygon (Accessible to Passport Holders and Cryptoversal Members ONLY)'
         },
         {
             img: 'map_plan.png',
-            text: 'Fee-less minting'
-        },
-        {
-            img: 'map_plan.png',
-            text: 'Volume mining rewards for OpenSea transactions'
-        },
-        {
-            img: 'map_plan.png',
-            text: 'PAGE burning mechanic to Mint NFTBooks '
-        },
-        {
-            img: 'map_plan.png',
-            text: 'NFTBook Minter public launch'
+            text: 'NFT-Mediated Licenses of story elements'
         }
     ]
 }
 
 const roadmapQ2 = {
-    title: 'Q2, 2022',
+    title: '2023',
     img: 'calendar.png',
     contents: [
         {
             img: 'map_plan.png',
-            text: 'Add NFTBook contract builder for Publishers to create their own collections on Page Network'
-        },
-        {
-            img: 'map_plan.png',
-            text: ' NFTBook Bazaar marketplace & PAGE token on Cosmos w/Evmos'
+            text: 'Integration with NFT Bookstore, NFTBook Bazaar marketplace, and PAGE token on Cosmos w/Evmos'
         }
     ]
 }
 
 const faqs = [
-    { "title": "What does the NFTBook Minter do exactly?", "content": "Our Minter is built to enable you to read NFTBooks in your web browser. You also have the freedom to do whatever you want with your NFTBooks; sell them on any NFT marketplace, give them away, burn them or just keep all of them for yourself. Immutably stored on IPFS and minted to the Polygon blockchain, NFTBooks are forever. " },
-    { "title": "How do I connect to Matic Network for minting?", "content": "Go here for detailed instructions for adding Matic network to your Metamask: <a href='https://medium.com/stakingbits/setting-up-metamask-for-polygon-matic-network-838058f6d844' target='_blank'>https://medium.com/stakingbits/<br/>setting-up-metamask-for-polygon-matic-network-838058f6d844</a>" },
-    { "title": "How will people read my book?", "content": "Your NFTBook will have both an interactive reader that shows up on your OpenSea listing AND a link to view the IPFS page with your book." },
-    { "title": "Can anyone read my book?", "content": "Yes, all books minted will be open for anyone to read. No purchase required." },
-    { "title": "How do I list my NFTBooks for sale after minting?", "content": "After your book is minted, you will receive a link to view it on OpenSea. Go to OpenSea and follow their instructions for listing your NFT for sale: <a href='https://support.opensea.io/hc/en-us/articles/360063498333-How-do-I-sell-an-NFT-' target='_blank'>https://support.opensea.io/hc/en-us/articles/360063498333-How-do-I-sell-an-NFT-</a>" },
-    { "title": "What do I do if minted a book by mistake or need to make an edit?", "content": "Contact an @admin on the PageDAO discord if you need help removing an NFTBook after minting. <a href='Https://bit.ly/pagedao-discord' target='_blank'>Https://bit.ly/pagedao-discord</a>" },
-    { "title": "Why Matic NFTBooks? Why not Ethereum?", "content": "We chose to use the Polygon Network (Matic) because the gas fees are basically non-existent. We know that not all authors have the means to pay hundreds of $$$ just to mint their book, so we opted for a solution that enables 100% of our writing community to publish their works – and more of them!" },
-    { "title": "How can I get $MATIC to pay for gas to mint?", "content": "You will need $MATIC bridged over to Polygon network to pay for gas fees. You can follow the guide here for help through that process: <a href='https://medium.com/prepo/setting-up-metamask-and-getting-eth-matic-on-polygon-step-by-step-guide-fd55147a0f05' target='_blank'>https://medium.com/prepo/setting-up-metamask-and-getting-eth-matic-on-polygon-step-by-step-guide-fd55147a0f05</a>." }
+    { "title": "What does Ye Olde Wordler Village Minting Press do exactly?", "content": "Our Web3 Literary Minter enables Wordler Village community members to publish Web3 stories that can be read in any web browser. You also have the freedom to do whatever you want with your Web3 stories; sell them on any NFT marketplace, give them away, burn them, or just keep all of them for yourself. Immutably stored on IPFS and minted to the Polygon blockchain, Web3 stories are forever. " },
+    { "title": "How do I connect to Polygon network and $MATIC tokens for minting?", "content": "Go here for detailed instructions for adding Polygon network and its native $MATIC tokens to your Metamask: <a href='https://medium.com/stakingbits/setting-up-metamask-for-polygon-matic-network-838058f6d844' target='_blank'>https://medium.com/stakingbits/<br/>setting-up-metamask-for-polygon-matic-network-838058f6d844</a>" },
+    { "title": "How will people read my stories?", "content": "Your Web3 stories will have both an interactive reader that shows up on your OpenSea listing AND a link to view the IPFS page with your book." },
+    { "title": "Can anyone read my book?", "content": "With the current minting press, all stories will be open-access for anyone to read without purchase. An option to mint stories that are gated for owner-access may become available in the future." },
+    { "title": "How do I list my Web3 stories for sale after minting?", "content": "After your story is minted, you will receive a link to view it on OpenSea. Go to OpenSea and follow their instructions for listing your NFT for sale: <a href='https://support.opensea.io/hc/en-us/articles/360063498333-How-do-I-sell-an-NFT-' target='_blank'>https://support.opensea.io/hc/en-us/articles/360063498333-How-do-I-sell-an-NFT-</a>" },
+    { "title": "What do I do if minted a story by mistake or need to make an edit?", "content": "Contact an @admin on the PageDAO discord if you need help removing an NFTBook after minting. <a href='Https://bit.ly/pagedao-discord' target='_blank'>Https://bit.ly/pagedao-discord</a>" },
+    { "title": "Why are we publishing to the Polygon blockchain and using $MATIC? Why not Ethereum, Solana, or some other chain?", "content": "We chose to use the Polygon PoS blockchain for its low gas fees, ensuring the broadest possible access for authors and readers. Also, Polygon PoS is energy efficient and certified carbon-neutral with a plan to become carbon-positive by the end of 2022, making it a perfect choice for protecting our planet. We look forward to being able to provide additional blockchain options as Web3 publishing develops in the future." },
+    { "title": "How can I get $MATIC to pay for the gas fees required to mint?", "content": "Until we can implement gas-free minting, you will need $MATIC bridged over to Polygon network to pay for gas fees. You can follow the guide here for help through that process: <a href='https://medium.com/prepo/setting-up-metamask-and-getting-eth-matic-on-polygon-step-by-step-guide-fd55147a0f05' target='_blank'>https://medium.com/prepo/setting-up-metamask-and-getting-eth-matic-on-polygon-step-by-step-guide-fd55147a0f05</a>." },
+    { "title": "Who is behind this project?", "content": "Wordler Village is an experimental Web3 project organized by Author Greg R. Fishbone and launched through Cryptoversal Books using an innovative minter developed by PageDAO, the Metaverse Writer's Guild. " }
 ]
 
 const handleDragStart = (e) => e.preventDefault();
